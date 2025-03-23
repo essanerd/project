60bfbd8a5323c41c4dd23ae3feb10bcdd0f9fb0e
essanerd <reduxwolverine@gmail.com>
2025-03-23 16:59:57 -0400

    updates

--- src/set7/G1.java
+++ src/set7/G1.java
@@ -1,12 +1,26 @@
 package set7;
 
 import java.util.Scanner;
-
 public class G1 {
     public static void main(String[] args) {
-        int num = (int) java.lang.Math.round(java.lang.Math.random() * 100);
+        int saver;
+        int attempt;
+        int num = (int) java.lang.Math.round(java.lang.Math.random() * 20);
         Scanner scanner = new Scanner(System.in);
         System.out.println("A wizard has chosen a secret number between 1 and 20. You must guess the number, and the wizard will give you hints like “Too high!” or “Too low!” until you get it right!");
-        for (int i = 1;)
+        for (attempt = 0; attempt <= 3; attempt++) {
+            System.out.println("enter number");
+             saver = scanner.nextInt();
+
+        }  if ( saver == num) {
+            System.out.println("Congratulations you did it");
+        } else if (saver <= num) {
+            System.out.println("Wrong! higher");
+        } else {
+            System.out.println("Wrong! lower");
+        }
     }
+
+
 }
+
