b4f77277e8f72710ea0abbd5968b2cf6eddeb6ab
UkashaAhmed <syedukasha@hotmail.com>
2025-03-26 14:01:23 -0400

    updates

---
+++ src/set10/Q51.java
@@ -0,0 +1,21 @@
+package set10;
+
+public class Q51 {
+    public static void main(String[] args) {
+        int [] array = {5,10,9,8};
+        if (array[0] < array[1] && array[0] < array[2] && array[0] < array[3]){
+            System.out.println(array[0]);
+        }
+        else if (array[1] < array[2] && array[1] < array[0] && array[1] < array[3]) {
+            System.out.println(array[1]);
+
+        }
+        else if (array[2] < array[1] && array[2] < array[0] && array[2] < array[3]) {
+            System.out.println(array[2]);
+
+        }
+        else {
+            System.out.println(array[3]);
+        }
+    }
+}
---
+++ src/set10/Q52.java
@@ -0,0 +1,11 @@
+package set10;
+
+public class Q52 {
+    public static void main(String[] args) {
+        int [] array = {4,5,6,1};
+        if (array[0] ) {
+            System.out.println(array[0]);
+        }
+    }
+}
+
