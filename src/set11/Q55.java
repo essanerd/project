526c75dc7f8bf631467df8b6c5f9e6eee329b2b4
essanerd <reduxwolverine@gmail.com>
2025-03-27 17:57:03 -0400

    update

--- src/set11/Q55.java
+++ src/set11/Q55.java
@@ -5,12 +5,37 @@ import java.util.Scanner;
 
 public class Q55 {
     public static void main(String[] args) {
+
         int[] array = new int[5];
         Scanner object = new Scanner(System.in);
-        System.out.println("enter 4 numbers");
-        int nums = object.nextInt();
-        Arrays.fill(array,nums);
-        System.out.println(array);
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("enter number");
+            array[i] = object.nextInt();
+        }
+        System.out.println("you have entered these numbers " +array [0] + ", " + array[1] + ", " +  array[2] + ", " + array[3] + ", " + array[4]);
 
     }
 }
+
+/*
+    enter number:
+    5
+    enter number:
+    15
+    enter number:
+    52
+    enter number:
+    53
+    enter number:
+    55
+
+    You have entered:
+    5
+    15
+    52
+    53
+    55
+
+*/
