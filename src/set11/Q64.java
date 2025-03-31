5875be499d802c33ab32f29d18b4d3bfce75901b
UkashaAhmed <syedukasha@hotmail.com>
2025-03-31 12:41:09 -0400

    update

--- src/set11/Q64.java
+++ src/set11/Q64.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class Q64 {
     public static void main(String[] args) {
-        int i =0;
-        int[] array = {0};
+        int index = 0;
+
         Scanner object = new Scanner(System.in);
         System.out.println("enter the size of your array");
-        int nums1 = object.nextInt();
-        array.length=nums1;
-        for ( i = 0; i <= array.length; i++) {
+        int size = object.nextInt();
+        int[] array = new int[size];
+
+        for (index = 0; index < array.length; index++) {
             System.out.println("Enter number");
-            array[i] = object.nextInt();
+            array[index] = object.nextInt();
         }
+
         System.out.println("enter a number that you want to search in the array");
-        int helper = object.nextInt();
-        if (array[i] == helper){
-        System.out.println(array[i]);
-    }else{
+        int valueToSearch = object.nextInt();
+
+        for (index = 0; index < array.length; index++) {
+            if (array[index] == valueToSearch) {
+                System.out.println(array[index]);
+                return;
+            }
+        }
+
         System.out.println("Error! number not found");
     }
-
-}
 }
